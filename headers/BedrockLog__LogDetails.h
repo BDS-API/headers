96ee8feb9711fcc82a2e4c7b08dec7baf94e4e7b
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2019-12-18 23:38:06 +0000

    Fix ABI tags parsing

--- headers/BedrockLog__LogDetails.h
+++ headers/BedrockLog__LogDetails.h
@@ -21,7 +21,6 @@ public:
     void _filterLog(LogAreaID, unsigned int);
     void _log_va(LogAreaID, unsigned int, char const*, int, int, char const*, __va_list_tag *);
     void _appendLogEntryMetadata(std::string &, LogAreaID, unsigned int, std::string, int, int);
-    void _appendLogEntryMetadata(std::__cxx11::basic_string<char,std::char_traits<char>,std::allocator<char>> &,LogAreaID,unsigned int,std::__cxx11::basic_string<char,std::char_traits<char>,std::allocator<char>>,int,int)::{lambda(std::__cxx11::basic_string<char,std::char_traits<char>,std::allocator<char>> const&,std::__cxx11::basic_string<char,std::char_traits<char>,std::allocator<char>> const&)#1}::operator() const[abi:cxx11](std::__cxx11::basic_string<char,std::char_traits<char>,std::allocator<char>> const&,std::__cxx11::basic_string<char,std::char_traits<char>,std::allocator<char>> const&);
     void _setAppend(bool);
     void _setFlushImmediate(bool);
     void _setTimestamp(bool);
