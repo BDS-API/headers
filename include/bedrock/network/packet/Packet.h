0fe427f50f6978f601f02340da3dacfa9eecf848
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2020-01-11 00:30:35 +0000

    Map more classes

--- include/bedrock/network/packet/Packet.h
+++ include/bedrock/network/packet/Packet.h
@@ -1,10 +1,21 @@
 #pragma once
 
-using namespace RakNet;
-
 class Packet {
 
 public:
+    virtual Packet::~Packet();
+    virtual void disallowBatching(void)const;
 
+    void getReliability(void)const;
+    void getCompressible(void)const;
+    void writeWithHeader(unsigned char, BinaryStream &)const;
+    void readNoHeader(ReadOnlyBinaryStream &, unsigned char const&);
+    void handle(NetworkIdentifier const&, NetEventCallback &, std::shared_ptr<Packet> &);
     Packet(void);
+    void getClientSubId(void)const;
+    Packet(Packet const&);
+    void setClientSubId(unsigned char);
+    Packet(Compressibility);
+    void setReliable(void);
+    void setReliableOrdered(void);
 };
